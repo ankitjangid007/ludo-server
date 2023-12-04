db2a2e72391c167fc2c4d0fa29657c2b6ad91a9d
ajay.jangid <ajay.jangid@radixweb.com>
2023-12-04 15:59:48 +0530

    running battle api query chages

--- src/services/openBattle.service.js
+++ src/services/openBattle.service.js
@@ -149,15 +149,33 @@ export const addBattleParticipant = async (openBattleId, userId) => {
   }
 };
 
+// Service to update room code for an open battle
+export const updateRoomCode = async (openBattleId, roomCode) => {
+  try {
+    const openBattle = await OpenBattle.findById(openBattleId);
+    if (!openBattle) {
+      throw new Error("Open battle not found");
+    }
+    openBattle.roomCode = roomCode;
+    openBattle.status = "Running";
+    const updatedOpenBattle = await openBattle.save();
+
+    return updatedOpenBattle;
+  } catch (error) {
+    throw new Error("Could not update room code: " + error.message);
+  }
+};
+
 //  delete openBattle
 export const deleteOpenBattle = async (battleId) => {
   try {
-    const result = await Battle.findByIdAndDelete(battleId);
+    const result = await OpenBattle.findByIdAndDelete(battleId);
     return result;
   } catch (error) {
     throw new Error("Could not delete battle");
   }
 };
+
 
 // <-------------------------------------------New Apis service for battle ( Ajay )----------------------------------->
 // Create new battle service
@@ -172,89 +190,84 @@ export const createNewBattleByUserService = async (userId, battleInfo) => {
       ...battleInfo,
       totalPrize,
       userId,
-    });
+    })
   } catch (error) {
     throw new Error("Could not create open battle: " + error.message);
   }
-};
+}
 
 // Get all newlyCreated battles
-export const getAllCreatedBattleService = async (userId) => {
+export const getAllCreatedBattleService = async (userId, limit, skip) => {
   userId = new Types.ObjectId(userId);
   try {
     try {
       return await Battle.aggregate([
         {
-          $match: {
-            $and: [
+          '$match': {
+            '$and': [
               {
-                $or: [
+                '$or': [
                   {
-                    status: "Created",
-                  },
+                    'status': 'Created'
+                  }, {
+                    'status': 'Requested'
+                  }
+                ]
+              }, {
+                '$or': [
                   {
-                    status: "Requested",
-                  },
-                ],
-              },
-              {
-                $or: [
-                  {
-                    userId: userId,
-                  },
-                  {
-                    $or: [
+                    'userId': userId
+                  }, {
+                    '$or': [
                       {
-                        participant: userId,
-                      },
-                      {
-                        participant: null,
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        },
-        {
-          $lookup: {
-            from: "users",
-            localField: "userId",
-            foreignField: "_id",
-            as: "userInfo",
-          },
-        },
-        {
-          $lookup: {
-            from: "users",
-            localField: "participant",
-            foreignField: "_id",
-            as: "participantInfo",
-          },
-        },
-        {
-          $unwind: "$userInfo",
-        },
-        {
-          $unwind: {
-            path: "$participantInfo",
-            preserveNullAndEmptyArrays: true,
-          },
-        },
-        {
-          $sort: {
-            createdAt: -1,
-          },
-        },
-      ]);
+                        'participant': userId
+                      }, {
+                        'participant': null
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        }, {
+          '$lookup': {
+            'from': 'users',
+            'localField': 'userId',
+            'foreignField': '_id',
+            'as': 'userInfo'
+          }
+        }, {
+          '$lookup': {
+            'from': 'users',
+            'localField': 'participant',
+            'foreignField': '_id',
+            'as': 'participantInfo'
+          }
+        }, {
+          '$unwind': '$userInfo'
+        }, {
+          '$unwind': {
+            'path': '$participantInfo',
+            'preserveNullAndEmptyArrays': true
+          }
+        }, {
+          '$sort': {
+            'createdAt': -1
+          }
+        }, {
+          '$skip': skip
+        }, {
+          '$limit': limit
+        }
+      ])
     } catch (error) {
       throw new Error("Could not get requested battles" + error.message);
     }
   } catch (error) {
     throw new Error("Could not get created battles" + error.message);
   }
-};
+}
 
 // Get all requested battles
 // export const getAllRequestedBattleService = async (userId, limit, skip) => {
@@ -316,129 +329,41 @@ export const getAllCreatedBattleService = async (userId) => {
 // Get all running battles
 export const getAllRunningBattleService = async (userId, limit, skip) => {
   try {
-    return await Battle.aggregate([
-      {
-        $match: {
-          $and: [
-            { status: "Running" },
-            {
-              $and: [
-                { userId: { $neq: userId } }, // Replace userId with the actual user ID
-                {
-                  participant: { $neq: userId },
-                },
-              ],
-            },
-          ],
-        },
+    return await Battle.aggregate([{
+      $match: {
+        status: "Running",
+      }
+    },
+    {
+      $lookup: {
+        from: "Users", // Assuming your user collection is named "users"
+        localField: "userId",
+        foreignField: "_id",
+        as: "userDetails",
       },
-      {
-        $lookup: {
-          from: "Users", // Assuming your user collection is named "users"
-          localField: "userId",
-          foreignField: "_id",
-          as: "userDetails",
-        },
+    },
+    {
+      $lookup: {
+        from: "Users", // Assuming your user collection is named "users"
+        localField: "participant",
+        foreignField: "_id",
+        as: "participantDetails",
       },
-      {
-        $lookup: {
-          from: "Users", // Assuming your user collection is named "users"
-          localField: "participant",
-          foreignField: "_id",
-          as: "participantDetails",
-        },
-      },
-      {
-        $unwind: "$userDetails",
-      },
-      {
-        $unwind: "$participantDetails",
-      },
-      {
-        $skip: skip, // Replace with your desired skip value
-      },
-      {
-        $limit: limit, // Replace with your desired limit value
-      },
-    ]);
+    },
+    {
+      $unwind: "$userDetails",
+    },
+    {
+      $unwind: "$participantDetails",
+    },
+    {
+      $skip: skip, // Replace with your desired skip value
+    },
+    {
+      $limit: limit, // Replace with your desired limit value
+    },])
   } catch (error) {
     throw new Error("Could not get requested battles" + error.message);
   }
-};
+}
 
-// Service to get open battle by ID
-export const getBattleById = async (openBattleId) => {
-  const id = new Types.ObjectId(openBattleId);
-
-  try {
-    const openBattle = await Battle.aggregate([
-      {
-        $match: {
-          _id: id,
-        },
-      },
-      {
-        $lookup: {
-          from: "users",
-          localField: "userId",
-          foreignField: "_id",
-          as: "userDetail",
-          pipeline: [
-            {
-              $project: {
-                userName: 1,
-                _id: 1,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $lookup: {
-          from: "users",
-          localField: "participant",
-          foreignField: "_id",
-          as: "participantDetail",
-          pipeline: [
-            {
-              $project: {
-                userName: 1,
-                _id: 1,
-              },
-            },
-          ],
-        },
-      },
-      {
-        $unwind: {
-          path: "$userDetail",
-        },
-      },
-      {
-        $unwind: {
-          path: "$participantDetail",
-        },
-      },
-    ]);
-    return openBattle[0];
-  } catch (error) {
-    throw new Error("Could not retrieve open battle: " + error.message);
-  }
-};
-
-// Service to update room code for an open battle
-export const updateRoomCode = async (openBattleId, roomCode) => {
-  try {
-    const openBattle = await Battle.findById(openBattleId);
-    if (!openBattle) {
-      throw new Error("Open battle not found");
-    }
-    openBattle.roomCode = roomCode;
-    openBattle.status = "Running";
-    const updatedOpenBattle = await openBattle.save();
-
-    return updatedOpenBattle;
-  } catch (error) {
-    throw new Error("Could not update room code: " + error.message);
-  }
-};
