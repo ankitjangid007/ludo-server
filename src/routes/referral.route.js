f3cf151fdfc0a28ff1eb268bf57943debb45c6a5
ajay9299 <ajayjangid9299@gmail.com>
2023-12-30 22:47:04 +0530

    referral withdraw api added

--- src/routes/referral.route.js
+++ src/routes/referral.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middleware/VerifyToken.js";
-import { getAllReferralsWithWalletController } from "../controllers/referral.controller.js";
+import { getAllReferralsWithWalletController, withdrawReferralAmountController } from "../controllers/referral.controller.js";
 
 const router = express.Router();
 
@@ -8,6 +8,6 @@ const router = express.Router();
 router.get("/details", verifyToken, getAllReferralsWithWalletController);
 
 // Withdraw referral amount (Whenever user withdraw referral amount then add this referral amount into winning cash);
-router.post("/withdraw",verifyToken,)
+router.post("/withdraw", verifyToken, withdrawReferralAmountController);
 
 export default router;
